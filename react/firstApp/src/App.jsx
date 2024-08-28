99439e5b90aa17b4df76dd96ab8a71848d03f638
Anand Kushwaha <anandkushwaha1220@gmail.com>
2024-08-28 11:11:03 +0530

    showing Chatterly

--- react/firstApp/src/App.jsx
+++ react/firstApp/src/App.jsx
@@ -1,24 +1,24 @@
 import "./App.css";
-// import CardsTab from "./Chatterly/CardsTab";
+import CardsTab from "./Chatterly/CardsTab";
 
 // import Cards from "./Card/Card"
-import Name from "./name/Name";
+// import Name from "./name/Name";
 
 function App() {
   return (
     <>
 
-    {/* <CardsTab/> */}
+    <CardsTab/>
       {/* <Cards />
       <Cards />
       <Cards />
       <Cards /> */}
-      <Name name="Anand" color="Blue"/>
+      {/* <Name name="Anand" color="Blue"/>
       <Name name="Prince" color="Red"/>
       <Name name="Ramesh" color="Green"/>
       <Name name="Rohan" color="darkRed"/>
       <Name name="Shohan" color="darkPink"/>
-      <Name name="Ramu" color="yellow"/>
+      <Name name="Ramu" color="yellow"/> */}
 
     </>
   );

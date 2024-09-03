054da5fa5d74a638279b28038173e60b72166d40
Anand Kumar Kushwaha <127625667+AnandKushwaha814@users.noreply.github.com>
2024-09-03 18:15:57 +0530

    Change in GitApi

--- react/firstApp/src/App.jsx
+++ react/firstApp/src/App.jsx
@@ -1,12 +1,49 @@
 import "./App.css";
-import Random from "./Tasks/Random";
+// import Random from "./Tasks/Random";
+// import Navbar from "./Tasks/Navbar";
+// import Counter from "./Tasks/Counter";
+import GitApi from "./Tasks/GitApi";
+// import BackgroundChanger from "./name/BackgroundChanger";
+// import CardsTab from "./Chatterly/CardsTab";
 
+// import Cards from "./Card/Card"
+// import Name from "./name/Name";
+
+
+// import Like from "./name/Like";
+// import Todo from "./Tasks/Todo";
+// import Bmi from "./Tasks/Bmi";
 
 function App () {
   return (
     <>
 
-      <Random/>
+{/* <CardsTab/> */}
+      {/* <Cards />
+      <Cards />
+      <Cards />
+      <Cards /> */}
+      {/* <Name name="Anand" color="Blue"/>
+      <Name name="Prince" color="Red"/>
+      <Name name="Ramesh" color="Green"/>
+      <Name name="Rohan" color="darkRed"/>
+      <Name name="Shohan" color="darkPink"/>
+      <Name name="Ramu" color="yellow"/> */}
+      {/* <Counter/> */}
+     
+      {/* <BackgroundChanger/> */}
+      
+      {/* <Like/> */}
+
+
+      {/* <Navbar/> */}
+
+      {/* <Todo/> */}
+
+      {/* <Bmi/> */}
+
+      <GitApi/>
+      {/* <Random/> */}
 
     </>
   );

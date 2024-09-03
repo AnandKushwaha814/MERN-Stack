df8685989e02f627cd685dd202d4942cdd03787f
Anand Kumar Kushwaha <127625667+AnandKushwaha814@users.noreply.github.com>
2024-09-03 13:06:39 +0530

    created randfom jokes

--- react/firstApp/src/App.jsx
+++ react/firstApp/src/App.jsx
@@ -1,18 +1,4 @@
 import "./App.css";
-// import Navbar from "./Tasks/Navbar";
-// import Counter from "./Tasks/Counter";
-// import GitApi from "./Tasks/GitApi";
-// import BackgroundChanger from "./name/BackgroundChanger";
-// import CardsTab from "./Chatterly/CardsTab";
-
-// import Cards from "./Card/Card"
-// import Name from "./name/Name";
-
-
-// import Like from "./name/Like";
-// import Todo from "./Tasks/Todo";
-// import Bmi from "./Tasks/Bmi";
-
 import Random from "./Tasks/Random";
 
 
@@ -20,31 +6,6 @@ function App () {
   return (
     <>
 
-    {/* <CardsTab/> */}
-      {/* <Cards />
-      <Cards />
-      <Cards />
-      <Cards /> */}
-      {/* <Name name="Anand" color="Blue"/>
-      <Name name="Prince" color="Red"/>
-      <Name name="Ramesh" color="Green"/>
-      <Name name="Rohan" color="darkRed"/>
-      <Name name="Shohan" color="darkPink"/>
-      <Name name="Ramu" color="yellow"/> */}
-      {/* <Counter/> */}
-     
-      {/* <BackgroundChanger/> */}
-      
-      {/* <Like/> */}
-
-
-      {/* <Navbar/> */}
-
-      {/* <Todo/> */}
-
-      {/* <Bmi/> */}
-
-      {/* <GitApi/> */}
       <Random/>
 
     </>
